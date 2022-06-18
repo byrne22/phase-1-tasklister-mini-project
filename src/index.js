c7fadb2e11be236727ae719598d4cb8c547990be
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-18 11:40:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let inputForm = document.getElementById("create-task-form");
+  inputForm.addEventListener("submit", createNovelTask);
 });
+
+const createNovelTask = function (event) {
+  event.preventDefault();
+  let newTaskList = document.getElementById("tasks");
+  let novelTask = document.createElement("li");
+  //appending new text to the created list
+  let novelTaskDescription = document.getElementById("new-task-description");
+  novelTask.innerText = novelTaskDescription.value;
+  console.log(novelTask);
+  appendNovelTask(novelTask);
+  event.target.reset();
+};
+const erase = (event) => {
+  event.target.ul.remove();
+};
+
+const appendNovelTask = (task) => {
+  document.getElementById("tasks").appendChild(task);
+};
